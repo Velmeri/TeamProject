90b8f5a367707509b4df734ae99d1f0aa8321de6
Daniel <Danielmikhh@gmail.com>
2023-10-16 22:08:50 +0200

    Управление персонажам
    wasd

--- TeamProject/Assets/Code/PlayerController.cs
+++ TeamProject/Assets/Code/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    // I don't have much time so comments will come later :)
+    public float Speed;
+    private Vector2 Direction;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Direction.x = Input.GetAxisRaw("Horizontal");
+        Direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Called a fixed number of times per second.
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + Direction * Speed * Time.fixedDeltaTime);
     }
 }
