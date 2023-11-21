2ca843e2587e31de36901a715eaee5145c1c3bec
Daniel <Danielmikhh@gmail.com>
2023-11-21 04:26:30 +0100

    Победнная анимация
    Спустя пять часов, я добавил эту анимацию

--- TeamProject/Assets/Door.cs
+++ TeamProject/Assets/Door.cs
@@ -8,10 +8,13 @@ public class Door : MonoBehaviour
 
     public SpriteRenderer spriteRenderer;
     public Sprite OpenDoor;
+
+    public Animator animator;
+    float framesPerSecond = 30.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Animator animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,24 +22,39 @@ public class Door : MonoBehaviour
     {
         spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
 
+        if(animator.GetBool("Victory") == true){
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            int totalFrames = Mathf.RoundToInt(stateInfo.length * framesPerSecond);
+            int currentFrame = Mathf.RoundToInt(stateInfo.normalizedTime * totalFrames);
+            if (currentFrame == totalFrames - 1)
+            {
+                lvlFinished();
+            }
+        }
+        
+        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(Input.GetKeyDown(KeyCode.E) && GameObject.Find("Door").GetComponent<SpriteRenderer>().sprite == OpenDoor)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            animator.SetBool("Victory", true);
         }
     }
     private void OnTriggerStay2D(Collider2D col)
     {
         if (Input.GetKeyDown(KeyCode.E) && GameObject.Find("Door").GetComponent<SpriteRenderer>().sprite == OpenDoor)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            animator.SetBool("Victory", true);
         }
     }
     private void OnTriggerExit2D(Collider2D col)
     {
 
+    }
+
+    private void lvlFinished(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
