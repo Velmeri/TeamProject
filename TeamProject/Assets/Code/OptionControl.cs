9247ebb7f8a0ed2dcc2ca669d61484fe9f126651
nebesnayaa <s.nebesnaya5@gmail.com>
2023-12-12 10:04:46 +0200

    музикальна доріжка

--- TeamProject/Assets/Code/OptionControl.cs
+++ TeamProject/Assets/Code/OptionControl.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OptionControl : MonoBehaviour
 {
     public Slider slideMusic;
-
-
     public Slider slideSounds;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -30,4 +27,23 @@ public class OptionControl : MonoBehaviour
     {
         AudioListener.volume = slideSounds.value;
     }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void SetResolution()
+    {
+        Screen.SetResolution(1920, 1080, Screen.fullScreen);
+    }
+
+    public void SaveSettings()
+    {
+
+    }
+    public void LoadSettings()
+    {
+
+    }
+
 }
